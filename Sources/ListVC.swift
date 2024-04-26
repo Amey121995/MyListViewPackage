857cc31d07f066b9bb7f6848689219e11a0cb49b
Ameya <amey.dalvi@fintoo.in>
2024-04-26 22:08:35 +0530

    Minor Bug Fixes

--- Sources/ListVC.swift
+++ Sources/ListVC.swift
@@ -32,7 +32,7 @@ class ListVC: UIViewController {
     }
     
     private func setupTableView() {
-        self.tableView.register(UINib(nibName: "UserCell", bundle: Bundle.module), forCellReuseIdentifier: "UserCell")
+        self.tableView.register(UserCell.self, forCellReuseIdentifier: "UserCell")
         self.tableView.dataSource = self
         self.tableView.delegate = self
         self.data.removeAll()
@@ -81,16 +81,7 @@ extension ListVC: UITableViewDataSource, UITableViewDelegate{
         let cell = tableView.dequeueReusableCell(withIdentifier: "UserCell", for: indexPath) as! UserCell
         print("Dequeued cell type: \(type(of: cell))")
         cell.selectionStyle = .none
-        cell.lblUserNames.text = "\(data.firstName ?? "") \(data.lastName ?? "")"
-        if data.id == self.selectedUser?.id{
-            cell.viewUserCard.backgroundColor = .blue.withAlphaComponent(0.1)
-            cell.lblUserNames.textColor = .white
-        }
-        else
-        {
-            cell.viewUserCard.backgroundColor = .white
-            cell.lblUserNames.textColor = .black
-        }
+        cell.configure(data: data)
         return cell
     }
     
--- Sources/UserCell.swift
+++ Sources/UserCell.swift
@@ -7,18 +7,52 @@
 
 import UIKit
 class UserCell: UITableViewCell {
-    @IBOutlet weak var viewUserCard: UIView!
-    @IBOutlet weak var lblUserNames: UILabel!
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-        
-    }
+  private let viewUserCard: UIView = {
+    let view = UIView()
+    view.backgroundColor = .white
+    view.translatesAutoresizingMaskIntoConstraints = false
+    return view
+  }()
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
+  private let lblUserNames: UILabel = {
+    let label = UILabel()
+    label.font = UIFont.systemFont(ofSize: 16)
+    label.textColor = .black
+    label.translatesAutoresizingMaskIntoConstraints = false
+    return label
+  }()
 
-        // Configure the view for the selected state
-    }
- 
+  // MARK: - Initialization
+  override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+    super.init(style: style, reuseIdentifier: reuseIdentifier)
+    setupView()
+  }
+
+  required init?(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+
+  // MARK: - Setup Methods
+  private func setupView() {
+    contentView.addSubview(viewUserCard)
+    viewUserCard.addSubview(lblUserNames)
+
+    // Add constraints for viewUserCard and lblUserNames
+    NSLayoutConstraint.activate([
+      viewUserCard.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 8),
+      viewUserCard.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 8),
+      viewUserCard.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -8),
+      viewUserCard.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -8),
+
+      lblUserNames.topAnchor.constraint(equalTo: viewUserCard.topAnchor, constant: 8),
+      lblUserNames.leadingAnchor.constraint(equalTo: viewUserCard.leadingAnchor, constant: 16),
+      lblUserNames.trailingAnchor.constraint(equalTo: viewUserCard.trailingAnchor, constant: -16),
+      lblUserNames.bottomAnchor.constraint(equalTo: viewUserCard.bottomAnchor, constant: -8),
+    ])
+  }
+
+  // MARK: - Configure Cell
+  func configure(data: User) {
+    lblUserNames.text = "\(data.firstName ?? "") \(data.lastName ?? "")"
+  }
 }
